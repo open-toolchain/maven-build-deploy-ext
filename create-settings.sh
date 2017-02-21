4c035a6b8fc072b316afdd2f2ef4775101bd7232
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2017-02-21 13:08:21 -0500

    Initial commit
    Signed-off-by: Olivier Thomann <Olivier_Thomann@ca.ibm.com>

---
+++ create-settings.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2017. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+
+cat - >> $HOME/settings.xml <<EOF
+<settings>
+  <pluginGroups>
+    <pluginGroup>org.sonatype.plugins</pluginGroup>
+  </pluginGroups>
+EOF
+
+if [ ! -z "${MAVEN_MIRROR_URL}" ]; then
+cat - >> $HOME/settings.xml <<EOF
+  <profiles>
+    <profile>
+      <id>artifactory</id>
+      <repositories>
+        <repository>
+          <id>${MAVEN_NAME}</id>
+          <url>${MAVEN_MIRROR_URL}</url>
+          <snapshots>
+            <enabled>true</enabled>
+          </snapshots>
+        </repository>
+      </repositories>
+    </profile>
+  </profiles>
+  <activeProfiles>
+    <!--make the profile active all the time -->
+    <activeProfile>artifactory</activeProfile>
+  </activeProfiles>
+EOF
+
+fi
+
+cat - >> $HOME/settings.xml <<EOF
+  <servers>
+    <server>
+      <id>${MABEN_NAME}</id>
+      <username>${MAVEN_USER_ID}</username>
+      <password>${MAVEN_TOKEN}</password>
+    </server>
+  </servers>
+EOF
+
+
+cat - >> $HOME/settings.xml <<EOF
+</settings>
+EOF
---
+++ generate-maven-env.js
@@ -0,0 +1,38 @@
+/**
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2017. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ */
+/*eslint-env node */
+var fs = require('fs'); 
+var services = JSON.parse(fs.readFileSync(process.env.SERVICE_INSTANCE_FILE)).services;
+
+var maven_service = findServiceInstance(services, process.env.SERVICE_INSTANCE_TYPE, process.env.SERVICE_INSTANCE);
+if (maven_service) {
+    var id = maven_service.parameters.name,
+        url = maven_service.parameters.dashboard_url,
+        mirror_url = maven_service.parameters.mirror_url,
+        snapshot_url = maven_service.parameters.snapshot_url,
+        user_id = maven_service.parameters.user_id,
+        token = maven_service.parameters.token;
+    console.log('export MAVEN_NAME="' + id + '"');
+    console.log('export MAVEN_USER_ID="' + user_id + '"');
+    console.log('export MAVEN_TOKEN="' + token + '"');
+    console.log('export MAVEN_RELEASE_URL="' + url + '"');
+    console.log('export MAVEN_SNAPSHOT_URL="' + snapshot_url + '"');
+    console.log('export MAVEN_MIRROR_URL="' + mirror_url + '"');
+}
+
+function findServiceInstance(services, type, serviceName) {
+    var maven = services.filter(function (v) {
+        return v.service_id === type
+            && (serviceName  === '(default)'
+                || v.parameters && v.parameters.name === serviceName);
+    });
+    if (maven.length>0) {
+        return maven[0];
+    }
+}
