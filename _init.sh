739935d2afc881d6a3dee239c4b12969f1eb4dc9
Paul Webster <pwebster@ca.ibm.com>
2017-03-03 15:21:22 -0500

    API URL changes in different envs

--- _init.sh
+++ _init.sh
@@ -47,6 +47,7 @@ fi
 
 export PATH=/opt/IBM/node-v4.6.0/bin:$PATH
 
+export TOOLCHAINS_API=$( echo $PIPELINE_API_URL | sed 's/pipeline/toolchains/g' )
 export SERVICE_INSTANCE_FILE=/tmp/tc_services.json
 export SETTINGS_DIR=$HOME/.m2
 export SETTINGS_FILE=$SETTINGS_DIR/settings.xml
